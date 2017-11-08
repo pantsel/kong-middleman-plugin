66a331bcc102aa50149d7503e06016acad4f2b49
Roilan Cardoso <roilan.cardoso@chronwell.com>
2017-11-08 09:14:10 -0500

    Refactoring as per https://streamdata.io/blog/developing-an-helloworld-kong-plugin/

--- src/handler.lua
+++ src/handler.lua
@@ -1,12 +1,5 @@
-local responses = require "kong.tools.responses"
 local BasePlugin = require "kong.plugins.base_plugin"
-local http = require"socket.http"
-local ltn12 = require"ltn12"
-local JSON = require "kong.plugins.middleman.json"
-
-local get_headers = ngx.req.get_headers
-local get_uri_args = ngx.req.get_uri_args
-local get_body = ngx.req.get_body_data
+local access = require "kong.plugins.middleman.access"
 
 local MiddlemanHandler = BasePlugin:extend()
 
@@ -18,38 +11,7 @@ end
 
 function MiddlemanHandler:access(conf)
   MiddlemanHandler.super.access(self)
-  
-  local post_url = conf.url
-  local headers = get_headers()
-  local uri_args = get_uri_args()
-  local body_data = get_body()
-  
-  local raw_json_headers    = JSON:encode(headers)
-  local raw_json_uri_args    = JSON:encode(uri_args)
-  local raw_json_body_data    = JSON:encode(body_data)
-
-  local payload = [[ {"headers":]] .. raw_json_headers .. [[,"uri_args":]] .. raw_json_uri_args.. [[,"body_data":]] .. raw_json_body_data .. [[} ]]
-  
-  local response_body = { }
-  
-  local res, code, response_headers, status = http.request
-  {
-    url = post_url,
-    method = "POST",
-    headers =
-    {
-      ["Content-Type"] = "application/json",
-      ["Content-Length"] = payload:len()
-    },
-    source = ltn12.source.string(payload),
-    sink = ltn12.sink.table(response_body)
-  }
-  
-  
-  if code > 299 then
-	return responses.send(code,table.concat(response_body))
-  end
-  
+  return access.execute(conf)  
 end
 
 return MiddlemanHandler
